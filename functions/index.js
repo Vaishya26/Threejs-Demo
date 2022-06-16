f434c7c3bf93e01af8efab7f2ec9f395b9fd5da5
= <=>
2022-06-16 22:36:10 +0530

    Created Firebase Environment

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,34 @@
-const functions = require("firebase-functions");
+const functions = require('firebase-functions');
+const express = require('express');
+const cors = require("cors");
+const admin = require("firebase-admin");
+const app = express();
+const port = 7777;
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+var serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://ajnasuite.firebaseio.com"
+});
+
+
+
+// Serve Static Assets
+app.use(express.static(__dirname + '/static', { dotfiles: 'allow' }));
+app.use(express.static('public'));
+app.use('/three', express.static('public/three'));
+app.use(express.static('public/three'));
+
+app.use(cors({ origin: true }));
+
+app.get('/scene', (req, res) => {
+    res.render("scene.ejs");
+});
+
+app.listen(port, function () {
+    console.log("Listening to port " + port);
+});
+
+
+exports.app = functions.https.onRequest(app);
